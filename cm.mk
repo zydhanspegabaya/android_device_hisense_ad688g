a173c3a5935a5839861163a111b171c24f57bf38
Zydhan Linnar Putra <zydhanspegabaya@gmail.com>
2015-01-25 03:41:46 +0700

    change product name & release name

--- cm.mk
+++ cm.mk
@@ -1,11 +1,11 @@
-$(call inherit-product, device/motorola/condor/full_condor.mk)
+$(call inherit-product, device/hisense/ad688g/full_condor.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_condor
-PRODUCT_RELEASE_NAME := MOTO E
+PRODUCT_NAME := cm_ad688g
+PRODUCT_RELEASE_NAME := Andromax AD688H
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
